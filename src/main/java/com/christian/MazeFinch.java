d1ed399df9000c92a85c70a8160b0be00a803155
MlbMonster <cjinwoo.jung@gmail.com>
2025-06-05 11:15:41 -0700

    Update MazeFinch.java

--- src/main/java/com/christian/MazeFinch.java
+++ src/main/java/com/christian/MazeFinch.java
@@ -4,6 +4,7 @@ import com.birdbrain.Finch;
 public class MazeFinch {
     
     public static void main(String[] args) {
+
         Finch myFinch = new Finch();
 
         //1st move
@@ -15,19 +16,28 @@ public class MazeFinch {
         //2nd turn
         myFinch.setTurn("L", 90, 100);
         //3rd move
-        myFinch.setMove("F", 35, 80);
+        myFinch.setMove("F", 31, 80);
         //3rd turn
         myFinch.setTurn("R", 90, 100);
         //4th move
         myFinch.setMove("F", 60, 80);
         //4th turn
-        myFinch.setTurn("R", 95, 100);
+        myFinch.setTurn("R", 90, 100);
         //5th move
         myFinch.setMove("F", 125, 60);
         //5th turn
-        myFinch.setTurn("L", 90, 100);
+        myFinch.setTurn("R", 90, 100);
         //6th move
         myFinch.setMove("F", 20, 80);
+        //6th turn
+        myFinch.setTurn("L", 90, 100);
+        //7th move
+        myFinch.setMove("F", 20, 80);
+        //7th turn
+        myFinch.setTurn("L", 90, 100);
+        //8th move
+        myFinch.setMove("F", 20, 80);
+        
 
 
         //Stops the code
